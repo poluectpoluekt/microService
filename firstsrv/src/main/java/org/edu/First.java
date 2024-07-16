4c261307862264b4fb6060d19b3f6a7fc69a58cc
poluect.poluekt <poluect.poluekt@inbox.ru>
2024-07-17 00:27:55 +0700

    first commit:   - KafkaProducer   - KafkaConsumer   - Dockerfile + docker-compose

---
+++ firstsrv/src/main/java/org/edu/First.java
@@ -0,0 +1,12 @@
+package org.edu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class First {
+
+    public static void main(String[] args) {
+        SpringApplication.run(First.class, args);
+    }
+}
