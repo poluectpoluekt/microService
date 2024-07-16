29ea86c7b160ba56c8cccf9f2acd8fc4e37bea5c
poluect.poluekt <poluect.poluekt@inbox.ru>
2024-07-17 01:03:45 +0700

    edit
    edit config consumer

--- secsrv/src/main/java/org/edu/controller/ActionController.java
+++ secsrv/src/main/java/org/edu/controller/ActionController.java
@@ -6,7 +6,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@AllArgsConstructor
 @RequestMapping("/api/action")
 @RestController
 public class ActionController {
